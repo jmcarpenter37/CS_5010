b2ea8bf0df34eae786e0a3cc3a911b538d729993
Carpenter, John M <jmcarpenter@mitre.org>
2019-08-29 20:46:44 -0400

    Commiting output file and dict code

---
+++ Week1/AssignmentPartA.py
@@ -0,0 +1,31 @@
+# Author: John M Carpenter
+# Date: 8/29/2019
+# Class: CS 5010
+# Assignment: Python Part A
+import sys
+import os
+# sys.stdout = open("TestFile.txt" , 'a')
+# Prompt user for: First Name, Last Name, Age
+counter = 0 # Using the counter so once the fourth entry is complete the dictionary will be written to a .txt file
+dict = {} # Initialize an empty dictionary to add Key : Value pairs to it.
+while counter <= 3:
+    print("Please enter your first name or type 'stop' to cancel the program: ")
+    first = input()
+    first = first.strip() # Stripping the leading and trailing whitespace
+    print("Please enter your last name: ")
+    last = input()
+    last = last.strip() # Stripping the leading and trailing whitespace
+    print("Please enter your age: ")
+    age = input()
+    age = int(age.strip()) # Stripping the leading and trailing whitespace
+    # Concatenate the First name and Last name into a single string so it looks like "Last-First"
+    concat = last + "-" + first
+    # print("Dict pre: {}".format(dict)) Used to make sure the dictionary was updating
+    dict[concat] = age
+    # print("Dict post: {}".format(dict)) Used to make sure the dictionary was updating
+    counter +=1
+
+# Write the dictionary to a .txt file
+with open('myOutput.txt' , 'w') as f:
+    print(dict , file = f)
+

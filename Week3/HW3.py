4a30b0990a03cbc41a12af39f329a78ffe7a688e
Carpenter, John M <jmcarpenter@mitre.org>
2019-09-15 14:33:53 -0400

    Added more to HW 3

--- Week3/HW3.py
+++ Week3/HW3.py
@@ -87,3 +87,30 @@ print("The number of o's are: {}".format(o))
 print("The number of u's are: {}".format(u))
 
 #Q5
+
+# Create a long sentence of words
+sent = "Hello there my name is John Carpenter and I am a computer scientist and I a have dog named Otto and he is a Great Pyrenees"
+# Put the words into a list
+sentList = list(sent.split(" ")) # Put sentence into list split by a space since we are assuming no punctuation
+# print(sentList) # Checking to make sure a list was created and each word was separated by a " "
+# We want this so we can use the list comprehension loop
+[print(sentList[word], len(sentList[word]) ) for word in range(len(sentList))] # Using the list comprehension to store the word and it's length in a tuple.
+# As you can see each time a word in sentList is iterated through it prints the word and its length.
+array = [ sentList[word]  for word in range(len(sentList))]
+# Sorting the array by length
+def Sorting( array_string ):
+    sorted_list = sorted(array_string , key = len) # This function sorts the array from smallest to largest. Taking advantage of the already build in sorted() method
+    return sorted_list
+
+sort = Sorting(array)
+# Printing out word size tuples from smallest to largest using list comprehension
+print("##########PRINTING OUR WORDSIZE TUPLES FROM SMALLEST TO SHORTEST##########")
+[print(sort[word], len(sort[word]) ) for word in range(len(sentList))]
+# Print out only unique words
+print("##########PRINTING ONLY UNIQUE VALUES##########")
+unique = set(sort)
+print(unique)
+
+# Q6 In the PowerPoint slides describing "higher-order functions" (02-Python - Map Filter Reduce.pdf) there are three examples:
+# one illustrating the use of map, the next one illustrating the use of filter, and the last one illustrating the use of reduce.
+# Rewrite these three examples without using the map(), filter(), and functools.reduce() functions.

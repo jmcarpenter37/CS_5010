8731bc0e6afb1d454c03afd51067456bc7c50bb5
Carpenter, John M <jmcarpenter@mitre.org>
2019-09-29 07:35:09 -0400

    Sequential SEarch added and the output of that file is in a txt file

---
+++ Week4/SeqSearch.py
@@ -0,0 +1,40 @@
+# Author: John Carpenter
+# ID: jmc7dt
+# Course: CS 5021
+# Screen capture(JPEG) or copy & paste three runs of your programs.
+
+# Output:
+# The index of the element in the list
+# If the element is not in the list, the function should return a sentinel value
+# If the element is in the list multiple times, return the first position
+
+def seq_search(array , target):
+    pos = 0 # using pos object as a counter
+    found = False # the default is set to false becuase we haven't found anything yet
+    while pos < len(array): # begin to move through the array
+        if array[pos] == target:
+            return "Your target was: {}".format( array[pos]) , "The position of the target is: {}".format( pos)
+        pos += 1 # We still need to check if the target exists elsewhere in the array
+    return found , "Your target was not found in the array provided."
+
+# Testing
+list1 = [1,2,3,4,5,6,32,89,4,5]
+ret = seq_search(list1 , 3)
+print(ret)
+
+# Find 4 since it exists twice
+list1 = [1,2,3,4,5,6,32,89,4,5]
+ret = seq_search(list1 , 4) # This algorithm will always return minimum index for a value if the target appears more than twice in the array
+print(ret)
+
+# Find 89
+list1 = [1,2,3,4,5,6,32,89,4,5]
+ret = seq_search(list1 , 89)
+print(ret)
+
+# Find 9000
+list1 = [1,2,3,4,5,6,32,89,4,5]
+ret = seq_search(list1 , 9000)
+print(ret)
+
+
